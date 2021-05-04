96bb8af963417a1b228089b3d49fefa7b0a83d0d
Om Prakash Shah <om@ksystem.com.np>
2021-05-04 11:19:15 +0545

    First commit with PodSpec

---
+++ ComponentLib/ComponentLib/ComponentLib.h
@@ -0,0 +1,18 @@
+//
+//  ComponentLib.h
+//  ComponentLib
+//
+//  Created by Om Prakash Shah on 5/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComponentLib.
+FOUNDATION_EXPORT double ComponentLibVersionNumber;
+
+//! Project version string for ComponentLib.
+FOUNDATION_EXPORT const unsigned char ComponentLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentLib/PublicHeader.h>
+
+
